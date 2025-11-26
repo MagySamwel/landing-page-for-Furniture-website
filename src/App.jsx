a4e417979763f1868737a5a3b5efa9cd2650d225
MagySamwel <maggi1samwel@gmail.com>
2025-11-26 23:27:53 +0200

    Remove unused component imports from App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,6 @@
 import "./App.css"
-import Chair from "./components/chairs/Chair";
-import Choose from "./components/Choose/Choose";
-import Help from "./components/Help/Help";
 import Hero from "./components/hero/Hero";
 import NavBar from './components/navbar/NavBar';
-import Product from "./components/product/Product";
 function App() {
   return (
     <>
