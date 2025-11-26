3ea63fa67d918064e17cad953b05d6d9963a2943
MagySamwel <maggi1samwel@gmail.com>
2025-11-26 23:42:03 +0200

    Refactor App.jsx to uncomment and organize component imports and usage

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
 import "./App.css"
+import Chair from "./components/chairs/Chair";
+import Choose from "./components/Choose/Choose";
+import Help from "./components/Help/Help";
 import Hero from "./components/hero/Hero";
 import NavBar from './components/navbar/NavBar';
+import Product from "./components/product/Product";
 function App() {
   return (
     <>
     
     <NavBar/>
     <Hero/>
-    {/* <Product/>
+    <Product/>
     <Choose/>
     <Help></Help>
-    <Chair/> */}
+    <Chair/>
     </>
   )
 }
